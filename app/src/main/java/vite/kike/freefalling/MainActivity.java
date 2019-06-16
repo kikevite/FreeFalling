0cad9a5d9af1e6178a1a84a9f6d4621d1e3f208c
3MIP <brianbvf@gmail.com>
2019-06-16 18:13:58 +0200

    Version 1.0

--- app/src/main/java/vite/kike/freefalling/MainActivity.java
+++ app/src/main/java/vite/kike/freefalling/MainActivity.java
@@ -15,15 +15,15 @@ public class MainActivity extends AppCompatActivity {
     private Chronometer chronometer;
     private boolean isStart;
     private long ini;
-    private TextView distancia, tiempo;
+    private TextView distanciaTV, tiempoTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        distancia = findViewById(R.id.dist);
-        tiempo = findViewById(R.id.time);
+        distanciaTV = findViewById(R.id.dist);
+        tiempoTV = findViewById(R.id.time);
         chronometer = findViewById(R.id.chronometer);
         Button btn = findViewById(R.id.btn);
         btn.setBackgroundColor(Color.GREEN);
@@ -44,11 +44,11 @@ public class MainActivity extends AppCompatActivity {
             ((Button)view).setText(getString(R.string.start));
             view.setBackgroundColor(Color.GREEN);
             long totalTime = fin - ini;
-            double dist = 0.5 * 9.8 * (totalTime) * (totalTime) / 1000000.0;
+            double dist = 0.5 * 9.81 * (totalTime) * (totalTime) / 1000000.0;
             dist = Math.round(dist * 100.0) / 100.0;
-            distancia.setText(String.format("%s m", dist));
+            distanciaTV.setText(String.format("%s m", dist));
             double time = totalTime / 1000.0;
-            tiempo.setText(String.format("%s s", time));
+            tiempoTV.setText(String.format("%s s", time));
         } else {
             ini = System.currentTimeMillis();
             chronometer.setBase(SystemClock.elapsedRealtime());
@@ -56,8 +56,8 @@ public class MainActivity extends AppCompatActivity {
             isStart = true;
             ((Button)view).setText(getString(R.string.stop));
             view.setBackgroundColor(Color.RED);
-            distancia.setText("");
-            tiempo.setText("");
+            distanciaTV.setText("");
+            tiempoTV.setText("");
         }
     }
 }
